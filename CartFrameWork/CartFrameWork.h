1171d7269055eea939a16372b579f0b179d8b68e
Waqar Ahmed <waqar.ahmed@swenggco-software.com>
2020-02-03 15:46:30 +0500

    Custom Framework for coredata

---
+++ CartFrameWork/CartFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  CartFrameWork.h
+//  CartFrameWork
+//
+//  Created by Waqar Ahmed on 31/01/2020.
+//  Copyright © 2020 Starkling. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CartFrameWork.
+FOUNDATION_EXPORT double CartFrameWorkVersionNumber;
+
+//! Project version string for CartFrameWork.
+FOUNDATION_EXPORT const unsigned char CartFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CartFrameWork/PublicHeader.h>
+
+
